9a7aca9c96495e504291c04b3ffb4d0844ff7cf3
Pranavranmale <pranavranmale1010@gmail.com>
2025-10-29 13:03:24 +0530

    compliting all project

--- FrontEnd/my-react-app/vite.config.js
+++ FrontEnd/my-react-app/vite.config.js
@@ -17,10 +17,11 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
   server: {
     port: 5173, // you can change this port if needed
   },
